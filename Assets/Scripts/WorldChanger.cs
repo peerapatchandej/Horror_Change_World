ee26dc6653a04da539c24b66f32bca91520d12d3
peerapatchandej <peerapat.chandej@gmail.com>
2026-02-19 12:13:29 +0700

    Comment code for learn

--- Assets/Scripts/WorldChanger.cs
+++ Assets/Scripts/WorldChanger.cs
@@ -11,14 +11,17 @@ public class WorldChanger : MonoBehaviour
   [Header("Settings")]
   public float lookAwayThreshold = -0.5f;
 
+  /*
   private bool playerPassedTrigger = false;
   private bool worldHasChanged = false;
+  */
 
   private void Awake()
   {
     newChange.SetActive(false);
   }
 
+  /*
   private void OnTriggerEnter(Collider other)
   {
     if (other.CompareTag("Player"))
@@ -27,17 +30,20 @@ public class WorldChanger : MonoBehaviour
       Debug.Log("Step 1: Player passed trigger. Ready to switch when look away.");
     }
   }
+  */
 
   private void Update()
   {
+    /*
     if (worldHasChanged || !playerPassedTrigger) return;
+    */
 
     Vector3 directionToTarget = Vector3.Normalize(transform.position - player.position);
 
     float dot = Vector3.Dot(player.forward, directionToTarget);
     Debug.Log(dot);
 
-    if (dot < lookAwayThreshold)
+    if (dot <= lookAwayThreshold)
     {
       ExecuteChange();
     }
@@ -46,6 +52,8 @@ public class WorldChanger : MonoBehaviour
   private void ExecuteChange()
   {
     newChange.SetActive(true);
+    /*
     worldHasChanged = true;
+    */
   }
 }

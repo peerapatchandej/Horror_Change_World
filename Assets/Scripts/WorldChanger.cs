611ca0dda9d1bb8a359586b604ff6dcbf0ac8557
peerapatchandej <peerapat.chandej@gmail.com>
2026-02-26 18:14:30 +0700

    Uncomment

--- Assets/Scripts/WorldChanger.cs
+++ Assets/Scripts/WorldChanger.cs
@@ -11,17 +11,14 @@ public class WorldChanger : MonoBehaviour
   [Header("Settings")]
   public float lookAwayThreshold = -0.5f;
 
-  /*
   private bool playerPassedTrigger = false;
   private bool worldHasChanged = false;
-  */
 
   private void Awake()
   {
     newChange.SetActive(false);
   }
 
-  /*
   private void OnTriggerEnter(Collider other)
   {
     if (other.CompareTag("Player"))
@@ -30,13 +27,10 @@ public class WorldChanger : MonoBehaviour
       Debug.Log("Step 1: Player passed trigger. Ready to switch when look away.");
     }
   }
-  */
 
   private void Update()
   {
-    /*
     if (worldHasChanged || !playerPassedTrigger) return;
-    */
 
     Vector3 directionToTarget = Vector3.Normalize(transform.position - player.position);
 
@@ -52,8 +46,6 @@ public class WorldChanger : MonoBehaviour
   private void ExecuteChange()
   {
     newChange.SetActive(true);
-    /*
     worldHasChanged = true;
-    */
   }
 }
